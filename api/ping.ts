ffbfa19758ed8deb7049ac3a0287184b1fc2c65a
apark12321-ux <apark12321@gmail.com>
2026-06-13 14:53:13 +0900

    erase

--- api/ping.ts
+++
@@ -1,4 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ ok: true, ts: Date.now(), env: { td: !!process.env.TWELVEDATA_API_KEY, kiwoom: !!process.env.KIWOOM_APP_KEY, naver: !!process.env.NAVER_CLIENT_ID } });
-}
